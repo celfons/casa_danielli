7690d1c146f9abf8caa02d20789539af5bb9eed8
celfons <marcel.resende@yahoo.com.br>
2018-09-24 15:44:37 -0300

    remove plugin dont used

--- danielli/settings.py
+++ danielli/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_object_actions',
     'django_userforeignkey',
     'casa_danielli',
 ]
