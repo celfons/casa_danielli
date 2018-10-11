4ce97756a1c5ac0524b0f7044dbf697bb8e95f26
Marcel Fonseca <marcel.resende@yahoo.com.br>
2018-10-11 10:49:02 -0300

    Update settings.py

--- danielli/settings.py
+++ danielli/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'danielli.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'casa',
-        'USER': 'root',
-        'PASSWORD': '123',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
